d949cf480c65cf516cbc9befc5e06a4da698d948
Laurent G <laurentguinotte@gmail.com>
2018-11-27 17:41:56 +0100

    Create convenience constants + make class method new private

--- lib/to_decimal.rb
+++ lib/to_decimal.rb
@@ -1,2 +1,20 @@
 require_relative 'to_decimal/to_decimal_class'
 require_relative 'to_decimal/wrong_base_input_error'
+
+module ToDecimal
+  Base2 = Convertor.new(2).freeze
+  Base3 = Convertor.new(3)
+  Base4 = Convertor.new(4)
+  Base5 = Convertor.new(5)
+  Base6 = Convertor.new(6)
+  Base7 = Convertor.new(7)
+  Base8 = Convertor.new(8)
+  Base9 = Convertor.new(9)
+  Base10 = Convertor.new
+
+  class Convertor
+    class << self
+      private :new
+    end
+  end
+end
